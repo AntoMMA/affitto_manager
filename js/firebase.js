5f01891be3466eedfbd6e91c498f73f9dec47c94
AntoMMA <anto.99.pass@gmail.com>
2026-02-26 00:14:59 +0100

    Obscure sensitive Firebase config values
    Updated Firebase configuration to hide sensitive information.

--- js/firebase.js
+++ js/firebase.js
@@ -7,16 +7,18 @@ import {
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCo-RUqFOsQqHGsnXUkFVOjANqEFj6JTpg",
+  apiKey: "...",
   authDomain: "affitto-manager.firebaseapp.com",
   projectId: "affitto-manager",
   storageBucket: "affitto-manager.firebasestorage.app",
-  messagingSenderId: "961351310754",
-  appId: "1:961351310754:web:476e4e69f77adffb8766fb",
-  measurementId: "G-WNV2FXTS3E"
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// 🔥 FONDAMENTALE
+setPersistence(auth, browserLocalPersistence);
